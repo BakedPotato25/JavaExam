ab37b6378563a6e7f0e986b30d724bff46fd3310
SweetPotato <nguyenkhair2501@gmail.com>
2026-01-17 21:14:21 +0700

    Lam xong bai 1

---
+++ src/Bai_1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Bai_1 {
+    public static boolean checkNguyenTo(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            while (n <= 0) {
+                n = sc.nextInt();
+            }
+            // 1. So le
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+
+            // 2. So chan
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+
+            // 3. In ra day so nguyen to co trong day tu 1 den n
+            for (int i = 1; i <= n; i++) {
+                if (checkNguyenTo(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+
+            // 4. In ra day so fibonacci co trong day so tu 1 den n
+            int f0 = 0;
+            int f1 = 1;
+            if (n >= 1) System.out.print("1 ");
+            int next = f0 + f1;
+            while (next <= n) {
+                System.out.print(next + " ");
+                f0 = f1;
+                f1 = next;
+                next = f0 + f1;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
